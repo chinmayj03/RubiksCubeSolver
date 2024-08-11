793b996af27b6fe1289438bdb5620139f6302bbb
chinmayj03 <96608989+chinmayj03@users.noreply.github.com>
2024-08-11 23:35:34 +0530

    added 3d model n dfs/bfs solvers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#include "Cube3D.cpp"
+#include "DFSSolver.h"
+#include "BFSSolver.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Cube3D object;
+    //  object.randomShuffleCube();
+    //  object.print();
+
+    // if(!object.isSolved()) cout<<"\n UNSOLVED \n";
+    // object.fPrime();
+    // object.print();
+    // if(object.isSolved()) cout<<"\n SOLVED \n";
+
+    Cube3D cube;
+    cube.print();
+
+    vector<GenericRubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    // DFS Solver Testing _____________________
+    DFSSolver<Cube3D, Hash3d> dfsSolver(cube, 8);
+    vector<GenericRubiksCube::MOVE> solve_moves = dfsSolver.solve();
+
+    // BFS Solver Testing _____________________
+    // BFSSolver<Cube3D, Hash3d> bfsSolver(cube);
+    // vector<GenericRubiksCube::MOVE> solve_moves = bfsSolver.solve();
+
+    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    dfsSolver.rubiksCube.print();
+
     return 0;
 }
